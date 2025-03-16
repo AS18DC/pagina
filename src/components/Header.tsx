6f94861e72f1b1560eb8998a2f11995256c94b98
Ali.D <alidahoukc@gmail.com>
2025-03-16 16:55:44 -0400

    cambios de colores

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import logo from '../img/logo8.png';
+import logo from '../img/logo6.png';
 export default function Header() {
   return (
     <header className="text-center">
       <div className="flex justify-center margen-abajo">
-        <img src={logo} alt="Logo BAI automation" width={380} className="animate-pulse"/>
+        <img src={logo} alt="Logo BAI automation" width={305} className="animate-pulse"/>
       </div>
       <h1 className="text-4xl ibm-plex-serif-thin font-bold mb-4 low-margin text-black">
         Te ahorramos <span className="text-[#b89595]">horas</span> de trabajo con IA
@@ -18,7 +18,7 @@ export default function Header() {
       </div>
 
       <a href="https://cal.com/baiautomation/60-min-meeting">
-      <button className="bg-[#ab5c5c] drop-shadow-lg text-gray-200 mt-5 px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+      <button className="bg-[#b89595] text-gray-100 mt-5 px-6 py-3 rounded-lg hover:bg-[#4b1515] transition shadow-lg shadow-[#d4a5a5]/100">
         Agendar consultoría <span className="font-bold">GRATIS</span>
       </button>
         </a>
--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -8,7 +8,7 @@ interface ServiceCardProps {
 
 export default function ServiceCard({ title, description, Icon }: ServiceCardProps) {
   return (
-    <div className="bg-[#586576] p-6 drop-shadow-xl rounded-xl hover:scale-105 transition">
+    <div className="bg-[#586576] p-6 drop-shadow-xl rounded-xl hover:scale-105 hover:bg-[#748396] transition">
       <Icon className="w-8 h-8 mb-4 text-[#b89595]" />
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-gray-400">{description}</p>
